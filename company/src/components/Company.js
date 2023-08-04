ece0c070628f1b793db2431c840c6b248922d507
JolantaWro <jolanta.woronowska@gmail.com>
2023-08-04 07:53:24 +0200

    up company component

--- company/src/components/Company.js
+++ company/src/components/Company.js
@@ -5,17 +5,29 @@ const Company = ({ company }) => {
 
     return (
         <div className="content">
-            {company.name === "" ? null : <p className="text__title">{company.name}</p>}
-            {company.date === "" ? null : <p className="content__item text">Data ostatniego wpisu <b className='text'>{company.date}r.</b></p>}
-            {company.capital === "" ? null : <p className="content__item text">
-                Wysokość kapitału zakładowego {company.capital}</p>}
-            {company.codePKD === "" ? null : <p className="content__item text">
-                Branża: {company.codePKD[0].opis} {company.codePKD[0].kodDzial},{company.codePKD[0].kodKlasa},
-                {company.codePKD[0].kodPodklasa}</p>}
-            {company.results === "" ? null : <p className="content__item text">
-                Ostatnio opublikowane dane w KRS: <b>{company.results[company.results.length -1].zaOkresOdDo}</b></p>}
-            {company.results === "" ? null : <p className="content__item text">
-                Data ostatnio opublikowanych danych: {company.results[company.results.length -1].dataZlozenia}r.</p>}
+            {company.name === "" ? null : 
+                <p className="text__title">{company.name}</p>
+                }
+            {company.codePKD === "" ? null : 
+            <p className="content__item text">Branża:<br></br>{company.codePKD[0].opis} {company.codePKD[0].kodDzial},{company.codePKD[0].kodKlasa},{company.codePKD[0].kodPodklasa}</p>
+            }
+
+            {company.capital === "" ? null : 
+            <p className="content__item text">Wysokość kapitału zakładowego: {company.capital}</p>
+            }
+
+            {company.date === "" ? null : 
+            <p className="content__item text">Data ostatniego wpisu: <b className='text'>{company.date}r.</b></p>
+            }
+
+            {company.results === "" ? null : 
+            <p className="content__item text">Ostatnio opublikowane dane w KRS: <b>{company.results[company.results.length -1].zaOkresOdDo}</b></p>
+            }
+
+            {company.results === "" ? null : 
+            <p className="content__item text">Data ostatnio opublikowanych danych: {company.results[company.results.length -1].dataZlozenia}r.</p>
+            }
+
         </div>
     );
 };
